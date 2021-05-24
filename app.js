4b02331935d1dba901fbbe74f4a700a1e832459f
Neradkiller <daren1997gonzalez@gmail.com>
2021-05-24 13:33:08 -0400

    feat: :sparkles: endpoint postparau una botella

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const botellasRoutes = require('./routes/botellas')
 
 const app = express()
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+app.use('/public', express.static(`${__dirname}/storage/imgs`))
 
 app.use('/api/v1', botellasRoutes)
 
